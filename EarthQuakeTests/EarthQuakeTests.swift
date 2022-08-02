b4c618fae2b520c8339b3fb0f584a6f9aa029792
Peter Liaw <peter@ampula.io>
2022-08-02 11:14:04 -0700

    added unit tests

--- EarthQuakeTests/EarthQuakeTests.swift
+++ EarthQuakeTests/EarthQuakeTests.swift
@@ -18,13 +18,44 @@ class EarthQuakeTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
     
-    func testGetData() throws {
+    /// Test the basic getter method
+    func testDataService() throws {
         let now = Epoch.now()
-        let exp = expectation(description: "getData")
+        let exp = expectation(description: "testGetData")
         DataService.shared.getData(startTime: now.delta(days: -1), endTime: now) { result in
             switch result {
             case .success(let data):
+                print("\(data.features.count) records retrieved")
                 exp.fulfill()
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+        waitForExpectations(timeout: 10)
+    }
+    
+    /// Test getInitialData and getOlderData, to mimic user paging down through records
+    func testDataServiceMgr() throws {
+        let exp = expectation(description: "testDataServiceMgr")
+        DataServiceMgr.shared.getInitialData { result in
+            switch result {
+            case .success(let resp):
+                DataServiceMgr.shared.getOlderData { result in
+                    switch result {
+                    case .success(let resp):
+                        var lastTime = Int.max
+                        for i in resp {
+                            if i.properties.time > lastTime {
+                                XCTFail("values not in descending order")
+                                break
+                            }
+                            lastTime = i.properties.time
+                        }
+                        exp.fulfill()
+                    case .failure(let error):
+                        XCTFail(error.localizedDescription)
+                    }
+                }
             case .failure(let error):
                 XCTFail(error.localizedDescription)
             }
